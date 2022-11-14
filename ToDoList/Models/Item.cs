1f31975647809f70a836ca4513f8cbb1f34a791e
Brooke Kullberg <brookekullberg@gmail.com>
2022-11-14 14:44:34 -0800

    add working project

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> { };
+
+    public Item(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
+
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+}
