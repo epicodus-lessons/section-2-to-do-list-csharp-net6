20f82c81677816bb882eb5a394edc6f205a57827
Brooke Kullberg <brookekullberg@gmail.com>
2022-11-14 14:28:00 -0800

    add working project

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      Item starterItem = new Item("Add first item to To Do List");
+      return View(starterItem);
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/items")]
+    public ActionResult Create(string description)
+    {
+      Item myItem = new Item(description);
+      return View("Index", myItem);
+    }
+
+  }
+}
