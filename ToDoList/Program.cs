1c9ca38319e12bce1ed91736cd2089495345d8da
Brooke Kullberg <brookekullberg@gmail.com>
2022-11-14 14:49:34 -0800

    add working project

---
+++ ToDoList/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ToDoList
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+      builder.Services.AddControllersWithViews();
+
+      WebApplication app = builder.Build();
+
+    //   app.UseDeveloperExceptionPage();
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+
+      app.UseRouting();
+
+      app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+      );
+
+      app.Run();
+    }
+  }
+}
